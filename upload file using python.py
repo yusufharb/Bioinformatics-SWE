1549dadd0fd4e23838e1b744ebfbd98a6aa983a2
Yusuf Harb <59343058+yusufharb@users.noreply.github.com>
2023-01-27 23:42:16 +0200

    Add files via upload

---
+++ upload file using python.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfile 
+import time
+
+ws = Tk()
+ws.title('PythonGuides')
+ws.geometry('400x200') 
+
+
+def open_file():
+    file_path = askopenfile(mode='r', filetypes=[('fastq Files', '*fastq')])
+    if file_path is not None:
+        pass
+
+
+def uploadFiles():
+    pb1 = Progressbar(
+        ws, 
+        orient=HORIZONTAL, 
+        length=300, 
+        mode='determinate'
+        )
+    pb1.grid(row=4, columnspan=3, pady=20)
+    for i in range(5):
+        ws.update_idletasks()
+        pb1['value'] += 20
+        time.sleep(1)
+    pb1.destroy()
+    Label(ws, text='File Uploaded Successfully!', foreground='green').grid(row=4, columnspan=3, pady=10)
+        
+    
+    
+adhar = Label(
+    ws, 
+    text='Upload fastq file '
+    )
+adhar.grid(row=0, column=0, padx=10)
+
+adharbtn = Button(
+    ws, 
+    text ='Choose File', 
+    command = lambda:open_file()
+    ) 
+adharbtn.grid(row=0, column=1)
+
+upld = Button(
+    ws, 
+    text='Upload Files', 
+    command=uploadFiles
+    )
+upld.grid(row=3, columnspan=3, pady=10)
+
+
+
+ws.mainloop()
